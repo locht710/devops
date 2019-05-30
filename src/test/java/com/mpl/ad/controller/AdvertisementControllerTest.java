cb540952b2789f82b983859751026c792c70444e
Hoang Trong Loc <locht3@fsoft.com.vn>
2019-05-30 10:48:43 +0700

    Complete apis

---
+++ src/test/java/com/mpl/ad/controller/AdvertisementControllerTest.java
@@ -0,0 +1,15 @@
+package com.mpl.ad.controller;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class AdvertisementControllerTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void getAllAd() {
+    }
+}
